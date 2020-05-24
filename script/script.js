0a8a53a0028f9eefaa38fe4096ec41ef5c3c5129
unknown <hugo.mar.310@gmail.com>
2020-05-24 14:13:30 -0500

    Tarea, falta redefinir diseño de estados y llamadas

--- script/script.js
+++ script/script.js
@@ -1,156 +1,66 @@
 //Esta función es para duplicar el DIV de Chats
 function duplicate(){
-            var str_html='';
-           var deb =document.querySelector('.copy').innerHTML;
-           str_html+=deb;
-           console.log(str_html) //grabar en consola
-           document.querySelector('body').innerHTML += str_html;
+ var str_html='';
+ //la variable deb se trae todos los div de la clase copy
+ // chats, estados y llamadas"
+ var deb =document.querySelectorAll('.copy');
+ for(i=0; i<deb.length; i++){
+   // la variable activo nos dirá cuál elemento
+   // está seleccionado "chats, estados o llamadas"
+   var activo = deb[i].style.display;
+  //solo al elemento que esté activo se le agregan copias
+  // de su contenido
+   if (activo=="block"){
+    str_html = document.querySelector('.copy-'+i).innerHTML;
+    console.log(str_html) //grabar en consola
+    document.querySelector('.copy-'+i).innerHTML += str_html;
+   }
+  
 
-       }
+ }
+
+
+}
 //Esta función activa DIV estados "como seleccionado"
 // y elimina la selección del resto
-       function activetab(){
-           var tabestados=document.querySelectorAll('#bottom-line');
-           for(var i=0; i<tabestados.length; i++){
-               tabestados[i].style.borderBottom = '3px solid green';
-            }
-           //console.log(tabestados[i]);
-           tabestados[1].style.borderBottom = '3px solid white';
-       }
+function activetab(renglon){
+ var tabestados=document.querySelectorAll('#bottom-line');
+ var edo = document.querySelectorAll('.copy');
+ for(var i=0; i<tabestados.length; i++){
+     tabestados[i].style.borderBottom = '3px solid green';
+     edo[i].style.display="none";
+
+  }
+
+ //al elemento que sea seleccionado se pinta línea blanca
+ // y además se muestra su contenido con display block
+ tabestados[renglon].style.borderBottom = '3px solid white';
+ edo[renglon].style.display="block";
+
+
+}
 //----------------------------------------------    
 
-//----------------------------------------------
 
-// activar chats con la función activvtab
-function activetabChat(){
-    var tabestados=document.querySelectorAll('#bottom-line');
-    for(var i=0; i<tabestados.length; i++){
-        tabestados[i].style.borderBottom = '3px solid green';
-     }
-    //console.log(tabestados[i]);
-    tabestados[0].style.borderBottom = '3px solid white';
-}
+//Aquí implementamos la nueva función "al cargar"
+document.addEventListener('DOMContentLoaded',function(){
+document.querySelectorAll('#bottom-line').forEach(function (div) {
+div.onclick=function(){
 
-//-----------------------------------------------
-//Activar llamadas con la función activetab
-function activetabCall(){
-    var tabestados=document.querySelectorAll('#bottom-line');
-    for(var i=0; i<tabestados.length; i++){
-        tabestados[i].style.borderBottom = '3px solid green';
-     }
+    //alert ("Entra a la función onclick de loader");
+    console.log(div.dataset.view)
     
-    tabestados[2].style.borderBottom = '3px solid white';
-}
+    //Aquí es donde Erick creó la magia completita
+    //solo llama a la función activetab y le envía el parametro
+    //contenido en el dataview
 
-//-----------------------------------------------
-
-
-//Esta función oculta el DIV estados
-function ocultarEstados(){
-    var edo = document.querySelectorAll('.copy-e');
-
-        for(var i=0; i<edo.length; i++){
-            edo[i].style.display="none";
-        }
-  }       
-//Esta función muestra el DIV estados
-  function mostrarEstados(){
-    var showe = document.querySelectorAll('.copy-e');
-
-        for(var i=0; i<showe.length; i++){
-            showe[i].style.display="block";
-        }
-  }
-//-----------------------------------------
-
-//-----------------------------------------
-//Esta función oculta el DIV de chats
-  function ocultarChats(){
-    var chat = document.querySelectorAll('.copy');
-
-        for(var i=0; i<chat.length; i++){
-            chat[i].style.display="none";
-        }
-  } 
-
-  //Esta función muestra el DIV Chats
-  function mostrarChats(){
-    var showc = document.querySelectorAll('.copy');
-
-        for(var i=0; i<showc.length; i++){
-            showc[i].style.display="block";
-        }
-  } 
-
-//-----------------------------------------------
-
-//Esta función oculta el DIV de Llamadas
-function ocultarCall(){
-    var call = document.querySelectorAll('.copy-c');
-
-        for(var i=0; i<call.length; i++){
-            call[i].style.display="none";
-        }
-  } 
-
-  //Esta función muestra el DIV Chats
-  function mostrarCall(){
-    var showcall = document.querySelectorAll('.copy-c');
-
-        for(var i=0; i<showcall.length; i++){
-            showcall[i].style.display="block";
-        }
-  } 
-
-//-----------------------------------------------
-
-
-//Función que muestra DIV de estados y oculta el de chats y llamadas, además
-//..activa la etiqueta "estados"
-  function showEhideChat(){
-      ocultarChats();
-      ocultarCall();
-      mostrarEstados();
-      activetab();
-  }
-//Función que muestra DIV de chats y oculta estados y llamadas, además
-//..activa la etiqueta de Chats
-  function ShowChatsHideEyLL(){
-      ocultarCall();
-      ocultarEstados();
-      mostrarChats();
-      activetabChat();
-  }
-//Función que muestra DIV de llamadas y oculta Chats y estados, además
-//..activa la etiqueta de Llamadas
-  function ShowCallHideCyE(){
-      ocultarChats();
-      ocultarEstados();
-      mostrarCall();
-      activetabCall();
-  }
-
-  //Aquí implementamos la nueva función "al cargar"
-  document.addEventListener('DOMContentLoaded',function(){
-      document.querySelectorAll('#bottom-line').forEach(function (div) {
-          div.onclick=function(){
-              console.log(div.dataset.view)
-              //Aquí van las funciones
-              if(div.dataset.view=="chats"){
-                ShowChatsHideEyLL();
-              }
-             
-              if(div.dataset.view=="estados"){
-                showEhideChat();
-              }
-              
-              if(div.dataset.view=="llamadas"){
-                ShowCallHideCyE();
-              }
-              
-          };
-      });
-      window.onscroll=function(){
-          this.console.log(window.scrollY)
-      }
-  });
+    activetab(div.dataset.view);
+  
+    
+};
+});
+/*
+window.onscroll=function(){
+this.console.log(window.scrollY)
+}*/
+});
